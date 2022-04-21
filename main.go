a5331d59e281ffc4fe377b3bd410b90d46e66dfb
Kolbe Prusa <kolbe@kprusa.dev>
2022-04-21 17:03:39 -0500

    Implemented Node, LinkState, Link, and NetworkTypology.

--- main.go
+++ main.go
@@ -1,3 +1,258 @@
 package main
 
+import (
+    "bufio"
+    "errors"
+    "fmt"
+    "io"
+    "log"
+    "regexp"
+    "strconv"
+    "strings"
+)
+
+// Node represents a network node in the ad-hoc network.
+type Node struct {
+    label string
+
+    // outputLog is where the Node will write all messages that it has sent.
+    outputLog io.Writer
+
+    // inputLog is where the Node will write all messages it has received.
+    inputLog io.Writer
+
+    // input represents the Node's wireless receiver.
+    input <-chan string
+
+    // output represents the Node's wireless transmitter.
+    output chan<- string
+}
+
+// run starts the Node "listening" for messages.
+func (n *Node) run(done <-chan struct{}) {
+    // Continuously listen for new messages until done received by Controller.
+    for {
+        select {
+        case in := <-n.input:
+            _, err := fmt.Fprintln(n.inputLog, in)
+            if err != nil {
+                log.Panicf("%s could not write out log: %s", n.label, err)
+            }
+            log.Printf("%s received msg: %s\n", n.label, in)
+
+        case <-done:
+            log.Printf("%s recevied done message", n.label)
+            return
+        }
+    }
+}
+
+// NewNode creates a network Node.
+func NewNode(input <-chan string, label string) *Node {
+    n := Node{}
+    n.label = label
+    n.input = input
+    n.output = make(chan<- string)
+    return &n
+}
+
+// LinkStatus represents whether a link is available or not.
+type LinkStatus string
+
+const (
+    // UP represents a link that is available.
+    UP LinkStatus = "UP"
+
+    // DOWN represents a link that is unavailable.
+    DOWN = "DOWN"
+)
+
+// LinkState represents a link's state at a given moment in time.
+type LinkState struct {
+    // time is the moment in time, inclusive, this state becomes valid.
+    time int
+
+    // status is the status of the link.
+    status LinkStatus
+
+    // fromNode is the source Node label.
+    fromNode string
+
+    // toNode is the destination Node label.
+    toNode string
+}
+
+func (l *LinkState) String() string {
+    return fmt.Sprintf("%d %s %s %s", l.time, l.status, l.fromNode, l.toNode)
+}
+
+func parseLinkState(state string) (*LinkState, error) {
+    ls := &LinkState{}
+
+    // Basic validation
+    splitState := strings.Split(state, " ")
+    if len(splitState) != 4 {
+        return nil, ErrParseLinkState{msg: "must be of the form: '{TIME} {UP | DOWN} {LABEL} {LABEL}'"}
+    }
+
+    // Parse time
+    time, err := strconv.Atoi(splitState[0])
+    if err != nil {
+        return nil, ErrParseLinkState{msg: fmt.Sprintf("time is not an integer: '%s'", splitState[0])}
+    }
+    ls.time = time
+
+    // Parse status
+    switch LinkStatus(splitState[1]) {
+    case UP:
+        ls.status = UP
+    case DOWN:
+        ls.status = DOWN
+    default:
+        return nil, ErrParseLinkState{msg: fmt.Sprintf("invalid status: '%s': must be {UP | DOWN}", splitState[1])}
+    }
+
+    // Parse labels
+    lre, err := regexp.Compile("^[A-Z]$")
+    if err != nil {
+        panic(err)
+    }
+    if !lre.Match([]byte(splitState[2])) {
+        return nil, ErrParseLinkState{msg: fmt.Sprintf("invalid label: '%s': must be '^[A-Z]$'", splitState[2])}
+    }
+    if !lre.Match([]byte(splitState[3])) {
+        return nil, ErrParseLinkState{msg: fmt.Sprintf("invalid label: '%s': must be '^[A-Z]$'", splitState[3])}
+    }
+    ls.fromNode = splitState[2]
+    ls.toNode = splitState[3]
+
+    return ls, nil
+}
+
+type Link struct {
+    // fromNode is the source Node label.
+    fromNode string
+
+    // toNode is the destination Node label.
+    toNode string
+
+    states []LinkState
+}
+
+// isUp determines whether the link is available at the given time.
+func (l *Link) isUp(time int) bool {
+    up := false
+    for _, state := range l.states {
+        if time >= state.time && state.status == UP {
+            up = true
+            continue
+        }
+        if time >= state.time && state.status == DOWN {
+            up = false
+            continue
+        }
+    }
+    return up
+}
+
+// QueryMsg enables the Controller to query the NetworkTopology to determine the state of a link at a given moment
+// in time.
+type QueryMsg struct {
+    // fromNodeLabel is the source of the link.
+    fromNodeLabel string
+
+    // toNodeLabel is the destination of the link.
+    toNodeLabel string
+
+    // timeQuantum is the moment in time to check the status of the link.
+    timeQuantum int
+}
+
+// NetworkTypology represents the ad-hoc network typology and is used by the Controller.
+type NetworkTypology struct {
+    links map[string]map[string]Link
+}
+
+type ErrParseLinkState struct {
+    msg string
+}
+
+func (e ErrParseLinkState) Error() string {
+    return fmt.Sprintf("parse link state: %s", e.msg)
+}
+
+func NewNetworkTypology(in io.ReadCloser) *NetworkTypology {
+    defer func(in io.ReadCloser) {
+        err := in.Close()
+        if err != nil {
+            log.Printf("unable to close input file: %s\n", err)
+        }
+    }(in)
+
+    n := &NetworkTypology{}
+    n.links = make(map[string]map[string]Link)
+
+    r := bufio.NewReader(in)
+    for {
+        line, err := r.ReadString('\n')
+        if err != nil {
+            if errors.Is(err, io.EOF) {
+                break
+            }
+            log.Panicf("invalid input topology: '%s'", err)
+        }
+        line = strings.TrimSuffix(line, "\n")
+
+        ls, err := parseLinkState(line)
+        if err != nil {
+            log.Fatalln(err)
+        }
+
+        // Add the new LinkState to the applicable link. If there is not a link, create one.
+        dsts, ok := n.links[ls.fromNode]
+        if !ok {
+            link := Link{fromNode: ls.fromNode, toNode: ls.toNode}
+            link.states = append(link.states, *ls)
+
+            srcMap := make(map[string]Link)
+            srcMap[ls.toNode] = link
+            n.links[ls.fromNode] = srcMap
+            continue
+        }
+        dst, ok := dsts[ls.toNode]
+        if !ok {
+            link := Link{fromNode: ls.fromNode, toNode: ls.toNode}
+            link.states = append(link.states, *ls)
+
+            dsts[ls.toNode] = link
+            continue
+        }
+
+        dst.states = append(dst.states, *ls)
+        dsts[ls.toNode] = dst
+    }
+
+    return n
+}
+
+// Query enables to Controller to determine the current link-state at a time quantum.
+func (n *NetworkTypology) Query(msg QueryMsg) bool {
+    links, in := n.links[msg.fromNodeLabel]
+    if !in {
+        return false
+    }
+
+    link, in := links[msg.toNodeLabel]
+    if !in {
+        return false
+    }
+
+    return link.isUp(msg.timeQuantum)
+}
+
+// Controller is aware of the entire network typology and acts as a wireless network.
+// Only used for the simulation (a real ad-hoc network would not have a centralized controller).
+type Controller struct {
+}
+
 func main() {}
---
+++ main_test.go
@@ -0,0 +1,403 @@
+package main
+
+import (
+    "io"
+    "os"
+    "reflect"
+    "testing"
+)
+
+func TestLinkState_String(t *testing.T) {
+    type fields struct {
+        time     int
+        status   LinkStatus
+        fromNode string
+        toNode   string
+    }
+    tests := []struct {
+        name   string
+        fields fields
+        want   string
+    }{
+        {
+            name: "valid",
+            fields: fields{
+                time:     10,
+                status:   UP,
+                fromNode: "A",
+                toNode:   "B",
+            },
+            want: "10 UP A B",
+        },
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            l := &LinkState{
+                time:     tt.fields.time,
+                status:   tt.fields.status,
+                fromNode: tt.fields.fromNode,
+                toNode:   tt.fields.toNode,
+            }
+            if got := l.String(); got != tt.want {
+                t.Errorf("String() = %v, want %v", got, tt.want)
+            }
+        })
+    }
+}
+
+func TestLink_isUp(t *testing.T) {
+    type fields struct {
+        fromNode string
+        toNode   string
+        states   []LinkState
+    }
+    type args struct {
+        time int
+    }
+    tests := []struct {
+        name   string
+        fields fields
+        args   args
+        want   bool
+    }{
+        {
+            name: "no states",
+            fields: fields{
+                fromNode: "A",
+                toNode:   "B",
+                states:   []LinkState{},
+            },
+            args: args{time: 0},
+            want: false,
+        },
+        {
+            name: "is up inclusive",
+            fields: fields{
+                fromNode: "A",
+                toNode:   "B",
+                states: []LinkState{
+                    {
+                        time:     1,
+                        status:   UP,
+                        fromNode: "A",
+                        toNode:   "B",
+                    },
+                },
+            },
+            args: args{time: 1},
+            want: true,
+        },
+        {
+            name: "up then down",
+            fields: fields{
+                fromNode: "A",
+                toNode:   "B",
+                states: []LinkState{
+                    {
+                        time:     1,
+                        status:   UP,
+                        fromNode: "A",
+                        toNode:   "B",
+                    },
+                    {
+                        time:     3,
+                        status:   DOWN,
+                        fromNode: "A",
+                        toNode:   "B",
+                    },
+                },
+            },
+            args: args{time: 4},
+            want: false,
+        },
+        {
+            name: "down then up",
+            fields: fields{
+                fromNode: "A",
+                toNode:   "B",
+                states: []LinkState{
+                    {
+                        time:     1,
+                        status:   DOWN,
+                        fromNode: "A",
+                        toNode:   "B",
+                    },
+                    {
+                        time:     3,
+                        status:   UP,
+                        fromNode: "A",
+                        toNode:   "B",
+                    },
+                },
+            },
+            args: args{time: 4},
+            want: true,
+        },
+        {
+            name: "between states",
+            fields: fields{
+                fromNode: "A",
+                toNode:   "B",
+                states: []LinkState{
+                    {
+                        time:     1,
+                        status:   DOWN,
+                        fromNode: "A",
+                        toNode:   "B",
+                    },
+                    {
+                        time:     3,
+                        status:   UP,
+                        fromNode: "A",
+                        toNode:   "B",
+                    },
+                },
+            },
+            args: args{time: 2},
+            want: false,
+        },
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            l := &Link{
+                fromNode: tt.fields.fromNode,
+                toNode:   tt.fields.toNode,
+                states:   tt.fields.states,
+            }
+            if got := l.isUp(tt.args.time); got != tt.want {
+                t.Errorf("isUp() = %v, want %v", got, tt.want)
+            }
+        })
+    }
+}
+
+func TestNetworkTypology_Query(t *testing.T) {
+    type fields struct {
+        links map[string]map[string]Link
+    }
+    type args struct {
+        msg QueryMsg
+    }
+    tests := []struct {
+        name   string
+        fields fields
+        args   args
+        want   bool
+    }{
+        {
+            name:   "is up",
+            fields: fields{links: NewNetworkTypology(getTopology1()).links},
+            args: args{msg: QueryMsg{
+                fromNodeLabel: "X",
+                toNodeLabel:   "Y",
+                timeQuantum:   10,
+            }},
+            want: true,
+        },
+        {
+            name:   "is down",
+            fields: fields{links: NewNetworkTypology(getTopology1()).links},
+            args: args{msg: QueryMsg{
+                fromNodeLabel: "X",
+                toNodeLabel:   "Y",
+                timeQuantum:   20,
+            }},
+            want: false,
+        },
+        {
+            name:   "is up end",
+            fields: fields{links: NewNetworkTypology(getTestData("./testdata/topology_1.txt")).links},
+            args: args{msg: QueryMsg{
+                fromNodeLabel: "Z",
+                toNodeLabel:   "X",
+                timeQuantum:   25,
+            }},
+            want: true,
+        },
+        {
+            name:   "label not in topology",
+            fields: fields{links: NewNetworkTypology(getTopology1()).links},
+            args: args{msg: QueryMsg{
+                fromNodeLabel: "Y",
+                toNodeLabel:   "X",
+                timeQuantum:   0,
+            }},
+            want: false,
+        },
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            n := &NetworkTypology{
+                links: tt.fields.links,
+            }
+            if got := n.Query(tt.args.msg); got != tt.want {
+                t.Errorf("Query() = %v, want %v", got, tt.want)
+            }
+        })
+    }
+}
+
+func getTestData(p string) io.ReadCloser {
+    f, err := os.Open(p)
+    if err != nil {
+        panic(err)
+    }
+    return f
+}
+
+func getTopology1() io.ReadCloser {
+    return getTestData("./testdata/topology_1.txt")
+}
+
+func TestNewNetworkTypology(t *testing.T) {
+    type args struct {
+        in io.ReadCloser
+    }
+    tests := []struct {
+        name string
+        args args
+        want *NetworkTypology
+    }{
+        {
+            name: "topology_1",
+            args: args{in: getTopology1()},
+            want: &NetworkTypology{
+                links: map[string]map[string]Link{
+                    "X": {
+                        "Y": {
+                            fromNode: "X",
+                            toNode:   "Y",
+                            states: []LinkState{
+                                {
+                                    time:     10,
+                                    status:   UP,
+                                    fromNode: "X",
+                                    toNode:   "Y",
+                                },
+                                {
+                                    time:     20,
+                                    status:   DOWN,
+                                    fromNode: "X",
+                                    toNode:   "Y",
+                                },
+                            },
+                        },
+                        "Z": {
+                            fromNode: "X",
+                            toNode:   "Z",
+                            states: []LinkState{
+                                {
+                                    time:     21,
+                                    status:   UP,
+                                    fromNode: "X",
+                                    toNode:   "Z",
+                                },
+                            },
+                        },
+                    },
+                    "Y": {
+                        "X": {
+                            fromNode: "Y",
+                            toNode:   "X",
+                            states: []LinkState{
+                                {
+                                    time:     10,
+                                    status:   UP,
+                                    fromNode: "Y",
+                                    toNode:   "X",
+                                },
+                                {
+                                    time:     20,
+                                    status:   DOWN,
+                                    fromNode: "Y",
+                                    toNode:   "X",
+                                },
+                            },
+                        },
+                    },
+                    "Z": {
+                        "X": {
+                            fromNode: "Z",
+                            toNode:   "X",
+                            states: []LinkState{
+                                {
+                                    time:     25,
+                                    status:   UP,
+                                    fromNode: "Z",
+                                    toNode:   "X",
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            if got := NewNetworkTypology(tt.args.in); !reflect.DeepEqual(got, tt.want) {
+                t.Errorf("NewNetworkTypology() = %v, want %v", got, tt.want)
+            }
+        })
+    }
+}
+
+func Test_parseLinkState(t *testing.T) {
+    type args struct {
+        state string
+    }
+    tests := []struct {
+        name    string
+        args    args
+        want    *LinkState
+        wantErr bool
+    }{
+        {
+            name: "valid",
+            args: args{state: "10 UP X Y"},
+            want: &LinkState{
+                time:     10,
+                status:   UP,
+                fromNode: "X",
+                toNode:   "Y",
+            },
+            wantErr: false,
+        },
+        {
+            name:    "invalid syntax",
+            args:    args{state: "10UP X Y"},
+            want:    nil,
+            wantErr: true,
+        },
+        {
+            name:    "invalid time",
+            args:    args{state: "x UP X Y"},
+            want:    nil,
+            wantErr: true,
+        },
+        {
+            name:    "invalid status",
+            args:    args{state: "1 x X Y"},
+            want:    nil,
+            wantErr: true,
+        },
+        {
+            name:    "invalid label",
+            args:    args{state: "1 UP Xa Y"},
+            want:    nil,
+            wantErr: true,
+        },
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            got, err := parseLinkState(tt.args.state)
+            if (err != nil) != tt.wantErr {
+                t.Errorf("parseLinkState() error = %v, wantErr %v", err, tt.wantErr)
+                return
+            }
+            if !reflect.DeepEqual(got, tt.want) {
+                t.Errorf("parseLinkState() got = %v, want %v", got, tt.want)
+            }
+        })
+    }
+}
