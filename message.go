10f88fe9035fa8a20836a69ee985f4e36d93748e
Kolbe Prusa <kolbe@kprusa.dev>
2022-04-21 22:46:02 -0500

    Messages now implement fmt.Stringer interface.

--- message.go
+++ message.go
@@ -1,15 +1,54 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func separatedString(items []NodeID, sep string) string {
+	var strs []string
+	for _, item := range items {
+		strs = append(strs, strconv.Itoa(int(item)))
+	}
+	return strings.Join(strs, sep)
+}
+
+type MsgType string
+
+const (
+	HelloType MsgType = "HELLO"
+	DataType  MsgType = "DATA"
+	TCType    MsgType = "TC"
+)
+
 // HelloMessage represents a HELLO OLSR message.
 type HelloMessage struct {
+	type_  MsgType
 	src    NodeID
 	unidir []NodeID
 	bidir  []NodeID
 	mpr    []NodeID
 }
 
+func (m *HelloMessage) Type() MsgType {
+	return HelloType
+}
+
+func (m *HelloMessage) String() string {
+	f := "* %d HELLO UNIDIR %s BIDIR %s MPR %s"
+	return fmt.Sprintf(
+		f,
+		m.src,
+		separatedString(m.unidir, " "),
+		separatedString(m.bidir, " "),
+		separatedString(m.mpr, " "),
+	)
+}
+
 // DataMessage represents a DATA OLSR message.
 type DataMessage struct {
+	type_   MsgType
 	src     NodeID
 	dst     NodeID
 	nxtHop  NodeID
@@ -17,10 +56,34 @@ type DataMessage struct {
 	data    string
 }
 
+func (m *DataMessage) Type() MsgType {
+	return DataType
+}
+
+func (m *DataMessage) String() string {
+	f := "%d %d DATA %d %d %s"
+	return fmt.Sprintf(f, m.nxtHop, m.fromnbr, m.src, m.dst, m.data)
+}
+
 // TCMessage represents a topology control (TC) OLSR message.
 type TCMessage struct {
+	type_  MsgType
 	src    NodeID
 	frombr NodeID
 	seq    uint
 	ms     []NodeID
+}
+
+func (m *TCMessage) Type() MsgType {
+	return TCType
+}
+
+func (m *TCMessage) String() string {
+	f := "* %d TC %d %d MS %s"
+	return fmt.Sprintf(f, m.frombr, m.src, m.seq, separatedString(m.ms, " "))
+}
+
+type Message interface {
+	fmt.Stringer
+	Type() MsgType
 }
---
+++ message_test.go
@@ -0,0 +1,129 @@
+package main
+
+import "testing"
+
+func TestTCMessage_String(t *testing.T) {
+	type fields struct {
+		type_  MsgType
+		src    NodeID
+		frombr NodeID
+		seq    uint
+		ms     []NodeID
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "check format",
+			fields: fields{
+				type_:  TCType,
+				src:    0,
+				frombr: 10,
+				seq:    2,
+				ms:     []NodeID{1, 2},
+			},
+			want: "* 10 TC 0 2 MS 1 2",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &TCMessage{
+				type_:  tt.fields.type_,
+				src:    tt.fields.src,
+				frombr: tt.fields.frombr,
+				seq:    tt.fields.seq,
+				ms:     tt.fields.ms,
+			}
+			if got := m.String(); got != tt.want {
+				t.Errorf("String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDataMessage_String(t *testing.T) {
+	type fields struct {
+		type_   MsgType
+		src     NodeID
+		dst     NodeID
+		nxtHop  NodeID
+		fromnbr NodeID
+		data    string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "check format",
+			fields: fields{
+				type_:   DataType,
+				src:     1,
+				dst:     4,
+				nxtHop:  3,
+				fromnbr: 9,
+				data:    "hello there",
+			},
+			want: "3 9 DATA 1 4 hello there",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &DataMessage{
+				type_:   tt.fields.type_,
+				src:     tt.fields.src,
+				dst:     tt.fields.dst,
+				nxtHop:  tt.fields.nxtHop,
+				fromnbr: tt.fields.fromnbr,
+				data:    tt.fields.data,
+			}
+			if got := m.String(); got != tt.want {
+				t.Errorf("String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHelloMessage_String(t *testing.T) {
+	type fields struct {
+		type_  MsgType
+		src    NodeID
+		unidir []NodeID
+		bidir  []NodeID
+		mpr    []NodeID
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "check format",
+			fields: fields{
+				type_:  HelloType,
+				src:    4,
+				unidir: []NodeID{1, 2, 3},
+				bidir:  []NodeID{5, 6},
+				mpr:    []NodeID{7, 8},
+			},
+			want: "* 4 HELLO UNIDIR 1 2 3 BIDIR 5 6 MPR 7 8",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &HelloMessage{
+				type_:  tt.fields.type_,
+				src:    tt.fields.src,
+				unidir: tt.fields.unidir,
+				bidir:  tt.fields.bidir,
+				mpr:    tt.fields.mpr,
+			}
+			if got := m.String(); got != tt.want {
+				t.Errorf("String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
